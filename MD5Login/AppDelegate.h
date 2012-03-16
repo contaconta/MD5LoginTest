765b9f2734123971e01f3ad9f8f889d720738083
Takanori Ogata <conta1031@gmail.com>
2012-03-16 20:46:59 +0900

    Initial Commit

---
+++ MD5Login/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MD5Login
+//
+//  Created by Takanori Ogata on 3/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MD5Login/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MD5Login
+//
+//  Created by Takanori Ogata on 3/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
